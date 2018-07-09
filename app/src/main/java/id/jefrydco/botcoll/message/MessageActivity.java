80ecb08675337c9062033325cf40668b4022f792
Jefry Dewangga <jefrydco@gmail.com>
2018-07-09 14:43:17 +0700

    Add login and signup activity.

--- app/src/main/java/id/jefrydco/botcoll/message/MessageActivity.java
+++ app/src/main/java/id/jefrydco/botcoll/message/MessageActivity.java
@@ -1,5 +1,6 @@
 package id.jefrydco.botcoll.message;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import id.jefrydco.botcoll.R;
+import id.jefrydco.botcoll.auth.LoginActivity;
 import id.jefrydco.botcoll.message.model.BaseMessage;
 import id.jefrydco.botcoll.message.model.BotMessage;
 import id.jefrydco.botcoll.message.model.UserMessage;
@@ -180,11 +184,39 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+        Intent intentToLoginActivityFromMessageActivity = new Intent(this, LoginActivity.class);
+        startActivity(intentToLoginActivityFromMessageActivity);
+
         setupAdapter();
         setupLayoutManager();
         setupRecyclerView();
         setupChatbox();
         setupNewMessage();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_logout:
+                Intent intentToLoginActivityFromMessageActivity = new Intent(this, LoginActivity.class);
+                startActivity(intentToLoginActivityFromMessageActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setupAdapter() {
